f3fb90174e01d4486730d2e55d03030fa718a1d6
J-T-Stewart <justin.troy.stewart@gmail.com>
2021-09-16 15:40:58 -0700

    Created / Styled Form Component

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -1,7 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import { Typography } from "@material-ui/core";
+import {
+  FormContainer,
+  StyledForm,
+  TextInput,
+  SubmitButton,
+  FileInput,
+} from "./styles";
+import FileBase from "react-file-base64";
 
 const Form = () => {
-  return <h1>FORM</h1>;
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const handleSubmit = () => {};
+  const clear = () => {};
+
+  return (
+    <FormContainer>
+      <StyledForm autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Typography variant="h6">Create a Post</Typography>
+        <TextInput
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextInput
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextInput
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextInput
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <FileInput>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </FileInput>
+        <SubmitButton
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </SubmitButton>
+        <SubmitButton
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </SubmitButton>
+      </StyledForm>
+    </FormContainer>
+  );
 };
 
 export default Form;
--- client/src/Components/Form/styles.js
+++ client/src/Components/Form/styles.js
@@ -1,1 +1,25 @@
 import styled from "styled-components";
+import { TextField, Button, Paper } from "@material-ui/core";
+
+export const FormContainer = styled(Paper)`
+  padding: 10px;
+`;
+
+export const TextInput = styled(TextField)`
+  margin: 5px;
+`;
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const FileInput = styled.div`
+  width: 97%;
+  margin: 10px 0;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-bottom: 10px;
+`;
