4b2e4b83e209124eab6af9f400f5cdbdd2ba222f
J-T-Stewart <justin.troy.stewart@gmail.com>
2021-09-27 22:47:24 -0700

    Like Functionality Complete

--- client/src/Components/Posts/Post/Post.js
+++ client/src/Components/Posts/Post/Post.js
@@ -12,7 +12,7 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
 import { useDispatch } from "react-redux";
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
@@ -51,7 +51,11 @@ const Post = ({ post, setCurrentId }) => {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(likePost(post._id))}
+        >
           {/* <ThumbUpAltIcon fontSize="small" /> */}
           Like {post.likeCount}
         </Button>
